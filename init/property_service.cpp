d4e8492cd603a138c5e1dcecc2eccf1e6089427d
xiaoleGun <1592501605@qq.com>
2026-08-20 22:56:32 +0800

    init: set properties to make SafetyNet pass

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1410,6 +1410,14 @@ static void ProcessBootconfig() {
     });
 }
 
+static void SetSafetyNetProps() {
+    InitPropertySet("ro.boot.flash.locked", "1");
+    InitPropertySet("ro.boot.vbmeta.device_state", "locked");
+    InitPropertySet("ro.boot.verifiedbootstate", "green");
+    InitPropertySet("ro.boot.veritymode", "enforcing");
+    InitPropertySet("ro.build.tags", "release-keys");
+}
+
 void PropertyInit() {
     selinux_callback cb;
     cb.func_audit = PropertyAuditCallback;
@@ -1422,6 +1430,12 @@ void PropertyInit() {
     }
     if (!property_info_area.LoadDefaultPath()) {
         LOG(FATAL) << "Failed to load serialized property info file";
+    }
+
+    // Report a valid verified boot chain before parsing androidboot properties,
+    // which are read-only once set.
+    if (!IsRecoveryMode()) {
+        SetSafetyNetProps();
     }
 
     // If arguments are passed both on the command line and in DT,
